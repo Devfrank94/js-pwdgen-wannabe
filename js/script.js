d074eb97d2eb9b91f19bc0459a814b1c0ecd9a67
Devfrank94 <francescomurro94@gmail.com>
2023-03-17 17:20:42 +0100

    write exercise track

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// CONSEGNA ESERCIZIO
+
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+// Suddivido in Step
+/*
+1. Chiedere il nome;
+2. Chiedere il cognome;
+3. Chiedere il colore preferito;
+4. Dichiaro le variabili dei dati ricevuti;
+5. Dichiaro una variabile che concateni i dati ricevuti nome, cognome e colore;
+6. Dichiaro variabile che di output che verrà stampata;
+7. Stampo sulla pagina HTML la variabile outputStr;
+*/
